1d68797449664934d2b9e387ddae2c8024f3fb95
HarshdeepBochare <bochareharshdeep2002@gmail.com>
2022-06-20 12:06:17 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -16,9 +16,9 @@ def hello_world():
     
     else:
         image_file = request.files['ximage']
-        image_path = "user_input_images/" + image_file.filename
+        image_path = "app/user_input_images/" + image_file.filename
         image_file.save(image_path)
-        model_new = keras.models.load_model('covpredd.h5')
+        model_new = keras.models.load_model('app/covpredd.h5')
         pred = cov_or_not(model_new, image_path)
         predicted = str(pred)
         
